fa97694a5c3ffbb2c4162aef4efd7eea3c3e3511
Amanuel <weditedi25@gmail.com>
2021-02-23 13:43:18 +0000

    dog photo  finished

---
+++ challenges/dog-photo-gallery/dogPhoto.js
@@ -0,0 +1,30 @@
+const parentContainer = document.getElementById("photoContainer");
+const dogPhotoAdder = document.getElementById("btnAdd");
+
+function dogPhotos() {
+  fetch(`https://dog.ceo/api/breeds/image/random`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      let dogImageList = document.createElement("li");
+      // dogImageList.className = "ImageList";
+      let dogImage = document.createElement("img");
+      dogImageList.style.listStyleType = "none";
+      dogImage.style.width = "220px";
+      dogImage.style.height = "200px";
+      dogImage.style.float = "left";
+      dogImage.style.marginRight = "8px";
+      dogImage.style.marginTop = "10px";
+
+      dogImage.src = data.message;
+      dogImageList.appendChild(dogImage);
+      parentContainer.appendChild(dogImageList);
+      console.log(dogImage);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+dogPhotoAdder.addEventListener("click", dogPhotos);
